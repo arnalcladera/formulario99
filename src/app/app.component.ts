763d12d616b867e509a188eb06c788a1810d6f99
arnalcladera <a_cladera@hotmail.com>
2019-10-30 10:56:54 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formulario99';
+  formulario: FormGroup;
+
+  constructor(frmBuilder: FormBuilder){
+    this.formulario = this.frmBuilder.group();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsModule,
+    ReactiveFormsModule
   ],
   imports: [
     BrowserModule,
